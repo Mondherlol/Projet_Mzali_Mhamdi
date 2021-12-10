c699e581058f0be5f9f2bb6689daa1f0d3bffd1b
Mondher <mondhermsp@gmail.com>
2021-12-10 01:44:41 +0100

    Statistique

--- src/app/composants/accueil/liste-regions/liste-regions.component.ts
+++ src/app/composants/accueil/liste-regions/liste-regions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Voyage } from 'src/app/model/voyage';
+import { RegionService } from 'src/app/services/region.service';
 import { VoyageService } from 'src/app/services/voyage.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { VoyageService } from 'src/app/services/voyage.service';
 export class ListeRegionsComponent implements OnInit {
   Voyages: Voyage[]=[];
   V:Voyage[]=[];
-  constructor(private voyageService:VoyageService) { }
+  constructor(private voyageService:VoyageService,private regionService:RegionService) { }
   Actif:String="Tout";
   test:boolean=true;
 filtrer(d:String){
@@ -30,6 +31,21 @@ filtrer(d:String){
 
 
   this.Actif=d;
+}
+stat(id:number){
+  this.voyageService.getVoyage().subscribe(data=>{
+    for(let r of data){
+      if(r.id==id){
+        if(r.nbVisites==NaN){
+          r.nbVisites=0;
+          this.voyageService.modifier(r.id,r).subscribe();
+
+        }
+        r.nbVisites++;
+        this.voyageService.modifier(r.id,r).subscribe();
+      }
+    }
+  })
 }
   ngOnInit(): void {
     // this.produitService.getProduits()
--- src/app/composants/admin/statistique/statistique.component.ts
+++ src/app/composants/admin/statistique/statistique.component.ts
@@ -6,30 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistique.component.css']
 })
 export class StatistiqueComponent implements OnInit {
-
-  constructor() { }
-
+  
   ngOnInit(): void {
+    this.teststorage();
   }
   public chartType: string = 'doughnut';
 
   public chartDatasets: Array<any> = [
-    { data: [300, 50, 100, 40, 120], label: 'My First dataset' }
+    { data: [300, 50, 100, 160], label: 'My First dataset' }
   ];
 
-  public chartLabels: Array<any> = ['Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'];
+  public chartLabels: Array<any> = ['Touristique', 'Historique', 'Traditionnelle', 'Paradisiaque'];
 
   public chartColors: Array<any> = [
     {
-      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
-      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774'],
+      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1'],
+      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5'],
       borderWidth: 2,
     }
   ];
 
   public chartOptions: any = {
     responsive: true
-}
-public chartClicked(e: any): void { }
+  };
+  public chartClicked(e: any): void { }
   public chartHovered(e: any): void { }
+
+
+
+ teststorage() {
+    if (typeof localStorage != 'undefined') {
+        // Récupération de la valeur dans web storage
+        var nbvisites = localStorage.getItem('visites');
+        // Vérification de la présence du compteur
+        if (nbvisites != null) {
+            // convertir chaine==>entier
+           var N=Number(nbvisites);
+        } else {
+            var N=1;
+
+        }
+
+        N++;
+        nbvisites=String(N);
+        // Stockage à nouveau en attendant la prochaine visite
+        localStorage.setItem('visites', nbvisites);
+        // Affichage dans la page
+        document.getElementById('test').innerHTML = "Le Nombre de visite est : "+nbvisites;
+    } else {
+        alert("localStorage n'est pas supporté");
+    }
 }
+}
+
